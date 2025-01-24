bc84baf4afe6a8bd4ec37fcecf167437cd4dc38f
Dmytro Levkovych <dtlevkovych@gmail.com>
2025-01-24 18:32:05 +0100

    test

---
+++ src/main/java/com/dtlev/lrnspb/LrnspbApplication.java
@@ -0,0 +1,13 @@
+package com.dtlev.lrnspb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LrnspbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LrnspbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtlev/lrnspb/LrnspbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtlev.lrnspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrnspbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
